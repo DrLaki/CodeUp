967c551170db7048267681c16c285ccb9014d195
Dejan Kosanovic <kosanovic.dejan96@gmail.com>
2018-05-27 19:18:47 +0200

    Artificial Intelligence page

---
+++ public_html/www.codeup.com/artificial_intelligence.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once("../codeup_res/helpers.php");
+require_once("../codeup_res/common_functions.php");
+
+require_once("../codeup_res/choose_controller.php");
+
+render('header', array(
+    'title' => "Algorithms",
+    'css' => track_style_sheets(),
+    'navigation' => $controller->header_navigation()
+));
+
+display_track('artificial_intelligence');
+
+render('footer');
+?>
