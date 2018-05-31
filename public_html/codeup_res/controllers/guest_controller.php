462abdbb130058e81b030c0aa72277e5ee98a35d
Dejan Kosanovic <kosanovic.dejan96@gmail.com>
2018-05-31 13:21:15 +0200

    Cleaner code

--- public_html/codeup_res/controllers/guest_controller.php
+++ public_html/codeup_res/controllers/guest_controller.php
@@ -1,4 +1,12 @@
 <?php
+define('MIN_PASSWORD_LEN', 8);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../codeup_res/php_mailer/src/Exception.php';
+require '../codeup_res/php_mailer/src/PHPMailer.php';
+require '../codeup_res/php_mailer/src/SMTP.php';
 
 class GuestController {
 
@@ -6,12 +14,12 @@ class GuestController {
 
     }
 
-    // navigacija koju treba gost da vidi
+    // guest navigation
     public function header_navigation() {
         return array('Home', 'Explore', 'Login', 'Signup');
     }
 
-    //style sheets za index.php
+    //style sheets for index.php
     public function index_style_sheets() {
         return array('css/style.css', 'css/index.css');
     }
@@ -21,63 +29,116 @@ class GuestController {
     }
 
 
+
+
+
+    //register helper functions
     public function register_style_sheets() {
         return array('css/style.css', 'css/login.css');
     }
 
     private function all_fields_are_set(){
-            return isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']);
+            return isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])
+                                             && isset($_POST['confirm_password']) && isset($_POST['country']);
     }
     private function all_field_are_filled() {
-            return !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_conf']);
+            return !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])
+                                              && !empty($_POST['confirm_password']) && !empty($_POST['country']);
     }
 
     private function password_confirm_matches_password() {
-        return $_POST['password'] == $_POST['password_conf'];
+        return $_POST['password'] == $_POST['confirm_password'];
     }
 
+    private function send_email($email, $registration_token) {
+        $url = "localhost/www.codeup.com/account_confirmation?email=$email&registration_token=$registration_token";
+
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure= 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465;
+        $mail->Username = 'pied.piper.codeup@gmail.com';
+        $mail->Password = 'pp_codeup';
+        $mail->SetFrom('no-reply@codeup.com');
+        $mail->Subject ='Account Confirmation';
+        $mail->Body = "Follow the given link to finish your registration: " . $url;
+        $mail->AddAddress($email);
+
+        if(!$mail->Send()) {
+            echo "Mailer error: " . $mail->ErrorInfo();
+        }
+        else {
+            echo "Email with confirmation details is sent to your email address.";
+        }
+    }
+    private function register_user($username, $password, $email, $country_id) {
+        require_once("../codeup_res/models/account_manager.php");
+        require_once("../codeup_res/models/country_manager.php");
+
+        if(AccountManager::username_exists($username)){
+            $error_message = "Username already taken.";
+            $countries = CountryManager::get_countries();
+            require_once("../codeup_res/views/signup.php");
+        }
+        else if(AccountManager::email_exists($email)) {
+            $error_message = "Account with that email already exists.";
+            $countries = CountryManager::get_countries();
+            require_once("../codeup_res/views/signup.php");
+        }
+        else if(strlen($password) < MIN_PASSWORD_LEN) {
+            $error_message = "Password must be at least " . MIN_PASSWORD_LEN . " characters long.";
+            $countries = CountryManager::get_countries();
+            require_once("../codeup_res/views/signup.php");
+        }
+        else {
+            $registration_token = hash('sha256', rand(1, 1000000000));
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            AccountManager::register_user($username, $password, $email, $country_id, $registration_token);
+            $this->send_email($email, $registration_token);
+        }
+    }
 
     public function register() {
+        require_once("../codeup_res/models/country_manager.php");
+
         $error_message = "";
 
         if (! $this->all_fields_are_set ()) {
             //user vists signup page for the first time, nothing has been
             //submitted yet
-                require_once("../codeup_res/views/signup.php");
+            $countries = CountryManager::get_countries();
+            require_once("../codeup_res/views/signup.php");
         }
         else if (!$this->all_field_are_filled()){
             $error_message = "Please, fill in the form.";
+            $countries = AccountManager::get_countries();
             require_once("../codeup_res/views/signup.php");
         }
         else if (!$this->password_confirm_matches_password()){
             $error_message = "Passwords do not match.";
+            $countries = AccountManager::get_countries();
             require_once("../codeup_res/views/signup.php");
         }
         else {
             //valid registration form, check if an account with the same
-            //username or password already email_exists
-            $username = htmlspecialchars($_POST['username']);
-            $email = htmlspecialchars($_POST['email']);
-            $password = htmlspecialchars($_POST['password']);
+            //username or password already exists
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $country_id = CountryManager::get_country_id($_POST['country']);
 
-            //preko user.php dohvatamo podatke iz baze
-            require_once("../codeup_res/models/account_manager.php");
-
-            //vraca prazan message ako je sve okej
-            // a u suprotnom vraca teskst greske
-            //TODO: throw exception instead of returning string when error occurs
-            $error_message = AccountManager::register_user($username, $email, $password);
-            if(empty($error_message)) {
-                //TODO: korisnik je registrovan, sacekati jos confirmation mail
-                echo '<p>You are registered.<p>';
-            }
-            else {
-                require_once("../codeup_res/views/signup.php");
-            }
+            $this->register_user($username, $password, $email, $country_id);
         }
     }
 
 
+
+
+
+
+    //login helper functions
     private function all_login_fields_are_set(){
         return isset($_POST['username']) && isset($_POST['password']);
     }
@@ -91,6 +152,26 @@ class GuestController {
         return array('css/style.css', 'css/login.css');
     }
 
+    private function verify_login($username, $password) {
+        require_once("../codeup_res/models/account_manager.php");
+
+        if(!AccountManager::is_user_active($username)) {
+            $error_message = "Please confirm your registration.";
+            require_once("../codeup_res/views/login.php");
+            return;
+        }
+
+        $hashed_password = AccountManager::get_hashed_password($username);
+        if(password_verify($password, $hashed_password)) {
+            $_SESSION['user_type'] = AccountManager::get_user_type($username);
+            header("Location: ./");
+        }
+        else {
+            $error_message = "Incorrect username or password.";
+            require_once("../codeup_res/views/login.php");
+        }
+    }
+
     public function login() {
         $error_message = "";
         if(! $this->all_login_fields_are_set()) {
@@ -101,18 +182,10 @@ class GuestController {
             require_once("../codeup_res/views/login.php");
         }
         else {
-            require_once("../codeup_res/models/account_manager.php");
-
             $username = $_POST['username'];
             $password = $_POST['password'];
 
-            if(AccountManager::login($username, $password)) {
-                header("Location: ./");
-            }
-            else {
-                $error_message = "Incorrect username or password.";
-                require_once("../codeup_res/views/login.php");
-            }
+            $this->verify_login($username, $password);
         }
     }
 }
