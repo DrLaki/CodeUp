97c6e6c3c38bdac205874198a7f881a9368fd4cf
Dejan Kosanovic <kosanovic.dejan96@gmail.com>
2018-06-08 13:58:53 +0200

    Added error404 and styled user support page

--- public_html/www.codeup.com/support.php
+++ public_html/www.codeup.com/support.php
@@ -6,7 +6,8 @@ require_once("../codeup_res/choose_controller.php");
 render('header', array(
     'title' => "Support",
     'css' => array(
-        'css/style.css'
+        'css/style.css',
+        'css/support.css'
     ),
     'navigation' => $controller->header_navigation()
 ));
